25bde974dbb8996ec5e95792ba3399778f2235d4
Satish Annigeri <satsih.annigeri@gmail.com>
2015-02-25 22:32:00 +0530

    Initial commit

---
+++ quad2.pyx
@@ -0,0 +1,14 @@
+from math import sin
+
+cpdef double f(double x):
+  return sin(x*x)
+
+def integrate(double a, double b, int N):
+  cdef int i
+  cdef double s, dx
+  s = 0.0
+  dx = float(b - a) / N
+  for i in range(N):
+    s += f(a + i*dx)
+  return s * dx
+
